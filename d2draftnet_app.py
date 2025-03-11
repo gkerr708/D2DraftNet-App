a53a45f0a2882f565bdf72ea2ac256549fec155d
gkerr708 <gkerr708@gmail.com>
2025-03-11 18:33:36 -0300

    mod to work on render

--- d2draftnet_app.py
+++ d2draftnet_app.py
@@ -1,6 +1,8 @@
+import os
 from d2draftnet_app import create_app
 
 app = create_app()
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.getenv("PORT", 10000))  # Render assigns a dynamic port
+    app.run(host="0.0.0.0", port=port, debug=True)
