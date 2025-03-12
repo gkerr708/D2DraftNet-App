a052f488c9d9a5a782a0d92d4ff49bac26e67ec8
gkerr708 <gkerr708@gmail.com>
2025-03-11 21:27:34 -0300

    Set debug to false

--- app.py
+++ app.py
@@ -4,5 +4,6 @@ from d2draftnet_app import create_app
 app = create_app()
 
 if __name__ == "__main__":
-    port = int(os.getenv("PORT", 10000))  # Render assigns a dynamic port
-    app.run(host="0.0.0.0", port=port, debug=True)
+    # Get the port from the environment (Render assigns this dynamically)
+    port = int(os.getenv("PORT", 10000))  # Default to 10000 if PORT is not set
+    app.run(host="0.0.0.0", port=port, debug=False)
